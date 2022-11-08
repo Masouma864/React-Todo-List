d37be67ec1763450c20f3d2d3dd5f06e3f123730
masouma <masouma2020@gmail.com>
2022-11-08 17:25:37 +0430

    Generating random ids for the todos list items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,22 +1,24 @@
 import React from "react"
 import TodosList from "./TodosList";
-import Header from "./Header"
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuidv4(),
                 title: "Setup development environment",
                 completed: true
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: "Develop website and add content",
                 completed: false
             },
             {
-                id: 3,
+               id: uuidv4(),
                 title: "Deploy to live server",
                 completed: false
             }
@@ -35,13 +37,36 @@ class TodoContainer extends React.Component {
             }),
           }))
       };
+      delTodo = id => {
+        this.setState({
+          todos: [
+            ...this.state.todos.filter(todo => {
+              return todo.id !== id;
+            })
+          ]
+        });
+      };
+      addTodoItem = title => {
+        const newTodo = {
+          id: 4,
+          title: title,
+          completed: false
+        };
+        this.setState({
+          todos: [...this.state.todos, newTodo]
+        });
+      };
     render() {
         return (
             <div>
-                <header/>
-                
-                <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
-            </div>
+            <Header />
+            <InputTodo addTodoProps={this.addTodoItem}/>
+            <TodosList
+              todos={this.state.todos}
+              handleChangeProps={this.handleChange}
+              deleteTodoProps={this.delTodo}
+            />
+          </div>
         );
 
     }
